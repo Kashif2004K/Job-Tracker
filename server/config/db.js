e5367b2ee0aa6e11f54c09a74c9ce3263285ed85
Kashif2004K <blackwatch4321@gmail.com>
2026-06-15 10:56:17 +0500

    changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB Connected Successfully");
-  } catch (error) {
-    console.log("Database connection failed:", error.message);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
 
-dotenv.config(); // load .env FIRST
+// load env FIRST
+dotenv.config();
 
-connectDB(); // then connect DB
+// connect DB
+connectDB();
 
 const app = express();
 
+// middleware
 app.use(express.json());
 
+// routes
+app.use("/api/users", userRoutes);
+
+// test route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+// start server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
